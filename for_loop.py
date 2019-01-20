060322048254c468c840ce15c47c6fbb03900142
Archit agarwal <architagarwal115@gmail.com>
2019-01-20 17:56:13 +0530

    _

---
+++ for_loop.py
@@ -0,0 +1,19 @@
+hero_list = ["superman", "batman", "spiderman", "ironman", "hulk"]
+
+for hero in hero_list:
+
+        print(len(hero_list))
+
+num_list = [1,2,40,50,2.5]
+
+for number in num_list:
+    if number ==50:
+        print(number)
+
+num_list = [1, 2, 40, 50, 2.5]
+
+for number in num_list:
+    if number%2 ==0:
+        print(number)
+
+
---
+++ week_05/list_class.py
@@ -0,0 +1,23 @@
+'''list_sports = ["cricket", "football", "basketball", "volleyball", "throwball"]
+
+print(list_sports[2])
+
+count = 0
+while count <= 100:
+    count = count + 1
+    if count%2 ==0:
+        print(count)'''
+
+n = int(input("ENTER A NUMBER: "))
+
+if n ==100:
+    print("number is equal to 100")
+else:
+    print("number is not equal to 100")
+
+
+
+
+
+
+
